2152097aa8a6165f9f3a686475021a7763b1f375
Alex Foster <123afoster@gmail.com>
2021-08-17 14:44:32 -0700

    read files to parse from text files

--- main.cpp
+++ main.cpp
@@ -13,14 +13,17 @@ files loadfiles()
 {
     files startupfiles;
 
-    startupfiles.asts.emplace_back("files/source_array_import.txt");
-    startupfiles.codes.emplace_back("files/source_array_import.pro");
-    startupfiles.asts.emplace_back("files/source_list_expand.txt");
-    startupfiles.codes.emplace_back("files/source_list_expand.pro");
-    startupfiles.asts.emplace_back("files/source_dft_model.txt");
-    startupfiles.codes.emplace_back("files/source_dft_model.pro");
-    startupfiles.asts.emplace_back("files/source_dft_multi.txt");
-    startupfiles.codes.emplace_back("files/source_dft_multi.pro");
+    std::vector<std::string> ast = loadfile("files2.txt");
+    std::vector<std::string> code = loadfile("files.txt");
+
+    for(std::string i : ast)
+    {
+        startupfiles.asts.emplace_back(i);
+    }
+    for(std::string i : code)
+    {
+        startupfiles.codes.emplace_back(i);
+    }
     return startupfiles;
 }
 
@@ -29,10 +32,10 @@ int main(int argc, char **argv)
     files sfiles = loadfiles();
     sourcetrail::SourcetrailDBWriter *writer = createdb();
     codedata output;
-    output = parseast(sfiles.asts.at(0), output);
-    output = parseast(sfiles.asts.at(1), output);
-    output = parseast(sfiles.asts.at(2), output);
-    output = parseast(sfiles.asts.at(3), output);
+    for(uint i = 0; i < sfiles.asts.size(); ++i)
+    {
+        output = parseast(sfiles.asts.at(i), output);
+    }
     parse(output, writer, sfiles);
     closedb(writer);
 }
