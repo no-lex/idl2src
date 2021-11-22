4958510340f16d331c22301cd434aa099d09d6ef
Alex Foster <123afoster@gmail.com>
2021-11-22 12:37:00 -0800

    remove unused loadfiles function

--- src/main.cpp
+++ src/main.cpp
@@ -9,24 +9,6 @@
 #include "parsegdl.h"
 #include "parsestructs.h"
 
-files loadfiles()
-{
-    files startupfiles;
-
-    std::vector<std::string> ast = startupfiles.loadfile("files.txt");
-    std::vector<std::string> code = startupfiles.loadfile("files2.txt");
-
-    for(std::string i : ast)
-    {
-        startupfiles.asts.emplace_back(i);
-    }
-    for(std::string i : code)
-    {
-        startupfiles.codes.emplace_back(i);
-    }
-    return startupfiles;
-}
-
 int main(int argc, char **argv)
 {
     //handle input arguments
